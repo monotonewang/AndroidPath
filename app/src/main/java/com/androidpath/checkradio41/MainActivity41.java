362969491a5e9778593090820872caa631c383d0
montotonewang <skarner@sina.com>
2016-11-04 15:26:27 +0800

    add -->spinner

---
+++ app/src/main/java/com/androidpath/checkradio41/MainActivity41.java
@@ -0,0 +1,143 @@
+package com.androidpath.checkradio41;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.androidpath.R;
+
+public class MainActivity41 extends Activity implements OnCheckedChangeListener, OnClickListener {
+
+	private CheckBox movieBox;//属性：引用，全局变量
+	private CheckBox codingBox;
+	private CheckBox meetBox;
+	private RadioGroup radioGroup;
+	private Button button;
+	
+	
+	//回调方法：生命周期方法，当系统创建好activity后就立马调用的
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//绑定布局：实际上系统会自动实例化布局文件中的view控件（new CheckBox(context, attrs)）
+		setContentView(R.layout.activity_main41);
+		
+		
+		//找控件的代码应该在绑定布局后
+		movieBox = (CheckBox) findViewById(R.id.movie_check);
+		codingBox = (CheckBox) findViewById(R.id.coding_check);
+		meetBox = (CheckBox) findViewById(R.id.meet_check);
+		button = (Button) findViewById(R.id.ok_button);
+		radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+		
+		//给radiogroup 注册监听器
+		/*
+		 * RadioGroup.OnCheckedChangeListener，
+		 * CompoundButton.OnCheckedChangeListener
+		 * 如果在同一个类中使用的接口同名了，这时就要让其中一个接口的名字写上包名+类名的全名，或者写上外部类名+接口名
+		 * 
+		 * */
+		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			//参数一是被选择的RadioGroup，参数二就是被选中的Radiobutton的id号
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				// TODO Auto-generated method stub
+				Log.e("checkedId", Integer.toHexString(checkedId)+"");
+				
+				if (checkedId == R.id.radio_all) {
+					//全选
+					
+					movieBox.setChecked(true);
+					codingBox.setChecked(true);
+					meetBox.setChecked(true);
+				}else if (checkedId == R.id.radio_none) {
+					//全不选
+					movieBox.setChecked(false);
+					codingBox.setChecked(false);
+					meetBox.setChecked(false);
+				}
+			}
+		});
+		
+		
+		
+		
+		movieBox.setOnCheckedChangeListener(this);
+		codingBox.setOnCheckedChangeListener(this);
+		meetBox.setOnCheckedChangeListener(this);
+	/*	
+		movieBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			//当checkbox的选中状态改变的时候就会回调，参数一是被选择或取消的view对象， 参数二是是否选中
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					Toast.makeText(MainActivity41.this, "选中了看电影", Toast.LENGTH_LONG).show();
+				}else {
+					Toast.makeText(MainActivity41.this, "取消了了看电影", Toast.LENGTH_LONG).show();
+				}
+			}
+		});*/
+		
+		
+		
+		button.setOnClickListener(this);
+	}
+
+	
+//回调方法
+	@Override
+	public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		// TODO Auto-generated method stub
+		/*if (buttonView.getId() == R.id.coding_check) {
+			if (isChecked) {
+				Toast.makeText(MainActivity41.this, "选中了敲代码", Toast.LENGTH_LONG).show();
+			}else {
+				Toast.makeText(MainActivity41.this, "取消了了敲代码", Toast.LENGTH_LONG).show();
+			}
+		}else if (buttonView.getId() == R.id.meet_check) {
+			if (isChecked) {
+				Toast.makeText(MainActivity41.this, "选中了约会", Toast.LENGTH_LONG).show();
+			}else {
+				Toast.makeText(MainActivity41.this, "取消了约会", Toast.LENGTH_LONG).show();
+			}
+		}*/
+		
+		if (isChecked) {
+			Toast.makeText(MainActivity41.this, "选中了"+buttonView.getText().toString(), Toast.LENGTH_LONG).show();
+		}else{
+			Toast.makeText(MainActivity41.this, "取消了"+buttonView.getText().toString(), Toast.LENGTH_LONG).show();
+			
+		}
+	}
+
+
+
+	//回调方法
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		//获取所有选中的选项：遍历所有的checkbox，判断是否选中
+		String msg = "";
+		if (movieBox.isChecked()) {
+			msg += movieBox.getText().toString();
+		}
+		if (codingBox.isChecked()) {
+			msg += "\n"+codingBox.getText().toString();
+		}
+		if (meetBox.isChecked()) {
+			msg += "\n"+meetBox.getText().toString();
+		}
+		
+		Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+		
+	}
+}
--- app/src/main/java/com/androidpath/main/MainActivity.java
+++ app/src/main/java/com/androidpath/main/MainActivity.java
@@ -8,12 +8,16 @@ import android.view.View;
 import com.androidpath.R;
 import com.androidpath.buttonbgcolor40.MainActivity40;
 import com.androidpath.buttonselector30.ButtonSelectorActivtiy3;
+import com.androidpath.checkradio41.MainActivity41;
 import com.androidpath.clickreponse31.ButtonTestActivity31;
 import com.androidpath.edittextdemo32.MainActivity32;
-import com.androidpath.widget.view.HandActivity;
 import com.androidpath.imageviewtest.MainActivity33;
 import com.androidpath.layoutdemo2.LayoutActivity2;
+import com.androidpath.spinnerdemo42.MainActivity42;
+import com.androidpath.spinnerdemo43.MainActivity43;
+import com.androidpath.spinnersimpleAdapter44.MainActivity44;
 import com.androidpath.viewtest34.MainActivity34;
+import com.androidpath.widget.view.HandActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,8 +58,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(handview);
                 break;
             case R.id.buttonbgcolor40:
-                Intent intent40=new Intent(MainActivity.this, MainActivity40.class);
+                Intent intent40 = new Intent(MainActivity.this, MainActivity40.class);
                 startActivity(intent40);
+            case R.id.checkRadio41:
+                Intent intent41 = new Intent(MainActivity.this, MainActivity41.class);
+                startActivity(intent41);
+            case R.id.spinnerDemo42:
+                Intent intent42 = new Intent(MainActivity.this, MainActivity42.class);
+                startActivity(intent42);
+            case R.id.spinnerDemo43:
+                Intent intent43 = new Intent(MainActivity.this, MainActivity43.class);
+                startActivity(intent43);
+            case R.id.spinnerSimpleAdapter44:
+                Intent intent44 = new Intent(MainActivity.this, MainActivity44.class);
+                startActivity(intent44);
             default:
 
                 break;
