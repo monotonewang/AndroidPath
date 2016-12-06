3806ab8f5643d5ec9963d9e3f0652de46478e188
montotone <zgjxfuqingwang@gmail.com>
2016-12-06 15:12:23 +0800

    alter-->seekbar volume

--- app/src/main/java/com/androidpath/view/widget/seekbar/VolumeSeekbar.java
+++ app/src/main/java/com/androidpath/view/widget/seekbar/VolumeSeekbar.java
@@ -2,15 +2,20 @@ package com.androidpath.view.widget.seekbar;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.SeekBar;
+
+import com.androidpath.R;
 
 /**
  * deso: AndroidPath TODO<br/>
@@ -26,6 +31,8 @@ public class VolumeSeekbar extends SeekBar {
     private int widthPixels;
     private int heightPixels;
     private Paint paint;
+    private int width;
+    private int height;
 
     public VolumeSeekbar(Context context) {
         this(context, null);
@@ -54,31 +61,30 @@ public class VolumeSeekbar extends SeekBar {
         paint.setStyle(Paint.Style.FILL);
     }
 
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = MeasureSpec.getSize(widthMeasureSpec);
+        height = MeasureSpec.getSize(heightMeasureSpec);
+//        Log.e(TAG, "onMeasure: width=" + width + "height=" + height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
 
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        Path path = new Path();
-//        path.moveTo(0, heightPixels);
-//        path.lineTo(widthPixels, heightPixels);
-//        path.lineTo(widthPixels, heightPixels - 100);
-//        path.close();
-        Path path2 = new Path();
-        path2.moveTo(90, 330);
-        path2.lineTo(150, 330);
-        path2.lineTo(120, 270);
-        path2.close();
-        canvas.drawPath(path2, paint);
-
-
-//        Bitmap bitmap = Bitmap.createBitmap(widthPixels, 200, Bitmap.Config.ARGB_8888);
-//        canvas.setBitmap(bitmap);
-//        if(bitmap!=null){
-//            Drawable drawable = new BitmapDrawable(context.getResources(), bitmap);
-////            this.setProgressDrawable(drawable);
-//        }else{
-//            Log.e(TAG, "onDraw: " + "drawable is null");
-//        }
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.volume);
+        Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+        setProgressDrawable(drawable);
     }
 
     @Override
