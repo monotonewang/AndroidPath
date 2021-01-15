830f24f2d9cda6de8021e21bcd0cbccd59d27f32
monotonewang <zgjxfuqingwang@gmail.com>
2021-01-15 13:21:13 +0800

    x

--- app/src/main/java/com/androidpath/activity/akservice/servicedemo241/MainActivity241.java
+++ app/src/main/java/com/androidpath/activity/akservice/servicedemo241/MainActivity241.java
@@ -12,51 +12,76 @@ import com.androidpath.activity.aabase.BaseActivity;
  * 认识Service：
  * 没有UI界面，并且长期运行与后台，所处的进程，我们通常叫做“服务进程”，类似于一个没有界面的Activity
  * 主要的功能：是用来实现一些长期处于后台工作的控件，比如媒体播放器，定时器等
- * 
+ * <p>
  * StartService的三个核心：
  * onCreate()
  * onStartCommand()
  * onDestory()
- * 
+ * <p>
  * 特点:
  * 用startService的方式启动的服务，与activity之间没有绑定关系，activity退出后，服务仍然在执行
- * 
+ * <p>
  * 注意:启动有一个onBind()方法必须重写，该方法是用于bindService时使用的，StartService时无需实现
- * 
+ * <p>
  * 具体步骤:
  * 1、声明一个Service的子类，重写onBind()方法
  * 2、在onCreate方法中，进行一个初始化操作，在onStartCommand()方法中进行一些核心的操作
  * 3、在配置清单文件中，注册该服务
  * 4、启动和停止服务
- *     停止服务：stopService
- *           在service内部调用stopSelf //停止自己
- * @author Ken
+ * 停止服务：stopService
+ * 在service内部调用stopSelf //停止自己
  *
+ * @author Ken
  */
 public class MainActivity241 extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		System.out.println("xxxxxxxxxxxxxxxxxxxxxxx onCreate MainActivity241");
-		setContentView(R.layout.activity_main241);
-		findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				System.out.println("xxxxxxxxxxxxxxxxxxxxxxx onClick start");
-				Intent intent = new Intent(MainActivity241.this, StartService.class);
-				intent.putExtra("name","weiwei");
-				startService(intent);
-			}
-		});
-		findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity241.this, StartService.class);
-				stopService(intent);
-			}
-		});
-	}
-	
+    boolean isRunning = true;
+
+    long last = System.currentTimeMillis();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxx onCreate MainActivity241");
+        setContentView(R.layout.activity_main241);
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("xxxxxxxxxxxxxxxxxxxxxxx onClick start");
+                Intent intent = new Intent(MainActivity241.this, StartService.class);
+                intent.putExtra("name", "weiwei");
+                startService(intent);
+//				startForegroundService(intent);
+            }
+        });
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity241.this, StartService.class);
+                stopService(intent);
+            }
+        });
+
+//		startThread();
+    }
+
+    private void startThread() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                System.out.println("xxxxxxxxxxxxx MainActivity241 thread run");
+                while (isRunning) {
+                    if (System.currentTimeMillis() - last > 1000) {
+                        last = System.currentTimeMillis();
+                        System.out.println("xxxxxxxxxxxx MainActivity241 Thread in " + System.currentTimeMillis() / 1000);
+                    }
+                }
+            }
+        };
+        thread.setName("MainActivity241 Thread");
+        thread.start();
+    }
+
 
 }
---
+++ app/src/main/java/com/androidpath/activity/akservice/servicedemo241/StartService.kt
@@ -0,0 +1,126 @@
+package com.androidpath.activity.akservice.servicedemo241
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.androidpath.R
+
+class StartService : Service() {
+    /**
+     * 初始化方法 -- 该方法只会被调用一次
+     */
+    override fun onCreate() {
+        super.onCreate()
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onCreate" + Thread.currentThread().name)
+    }
+
+    var isRunning = true
+    var last = System.currentTimeMillis()
+    var notificationId = 328989
+    var CHANNEL_ID = "CHANNEL_ID"
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    /**
+     * 该方法会被调用多次 -- 每启动一次service，该方法就会被调用一次
+     */
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        var name = ""
+        if (intent != null) name = intent.getStringExtra("name")
+        println("xxxxxxxxxxxxx begin thread run")
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                super.run()
+                println("xxxxxxxxxxxxx StartService thread run")
+
+                var url = "http://mp3.9ku.com/hot/2009/01-14/179021.mp3"
+                val mediaPlayer = MediaPlayer()
+                mediaPlayer.setDataSource(url)
+                mediaPlayer.prepareAsync()
+                mediaPlayer.setOnPreparedListener {
+                    println("xxxxxxxxxxxxx setOnPreparedListener thread run")
+                    it.start()
+                }
+
+                var flagss = 0;
+                // Create an explicit intent for an Activity in your app
+                val intent = Intent(this@StartService, MainActivity241::class.java).apply {
+                    flagss = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+
+                val pendingIntent: PendingIntent = PendingIntent.getActivity(this@StartService, 0, intent, 0)
+
+                var builder = NotificationCompat.Builder(this@StartService, "CHANNEL_ID")
+                        .setSmallIcon(R.drawable.a0)
+                        .setContentTitle("music")
+                        .setContentText("红尘滚滚来")
+//                        .setStyle(NotificationCompat.BigTextStyle()
+//                                .bigText("Much longer text that cannot fit one line..."))
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .addAction(R.drawable.a6t,"Previous",prevPendingIntent)
+                        .setContentIntent(pendingIntent)
+                createNotificationChannel()
+                NotificationManagerCompat.from(this@StartService).apply {
+                    notify(notificationId, builder.build())
+                }
+                while (isRunning) {
+                    if (System.currentTimeMillis() - last > 1000) {
+                        last = System.currentTimeMillis()
+                        println("xxxxxxxxxxxx StartService Thread in " + System.currentTimeMillis() / 1000)
+                    }
+                }
+            }
+        }
+//        stopSelf()
+        thread.name = "StartService"
+        thread.start()
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onStartCommand" + Thread.currentThread().name + "flags=" + flags + "startId=" + startId + "name=" + name)
+        //        Log.d("print", "--->onStartCommand" + Thread.currentThread().getName() + "flags=" + flags + "startId=" + startId+"name="+name);
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onDestroy" + Thread.currentThread().name)
+    }
+
+    override fun onBind(arg0: Intent): IBinder? {
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onBind" + Thread.currentThread().name)
+        return null
+    }
+
+    override fun onRebind(intent: Intent) {
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onRebind" + Thread.currentThread().name)
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" + "--->onUnbind" + Thread.currentThread().name)
+        return super.onUnbind(intent)
+    }
+}
